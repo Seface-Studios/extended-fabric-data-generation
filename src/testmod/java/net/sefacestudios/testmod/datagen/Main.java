c1691883a5a24c43e076510e027ab8e2ee222dac
Leonardo Luiz Gava <llgavamt@gmail.com>
2025-09-10 00:47:52 -0400

    add: start with CrowdinLanguageProvider

--- src/testmod/java/net/sefacestudios/testmod/datagen/Main.java
+++ src/testmod/java/net/sefacestudios/testmod/datagen/Main.java
@@ -2,6 +2,7 @@ package net.sefacestudios.testmod.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.sefacestudios.datagen_extras.provider.CrowdinLanguageProvider;
 import net.sefacestudios.datagen_extras.utils.ForgedModLoaders;
 import net.sefacestudios.testmod.datagen.provider.ModCompostablesDataMapProvider;
 import net.sefacestudios.testmod.datagen.provider.ModForgeBiomeModifierProvider;
@@ -18,6 +19,20 @@ public class Main implements DataGeneratorEntrypoint {
     pack.addProvider(ModFurnaceFuelsDataMapProvider::new);
     pack.addProvider(ModCompostablesDataMapProvider::new);
     pack.addProvider(ModForgeBiomeModifierProvider::new);
-    pack.addProvider((output, registriesFuture) -> new ModForgeBiomeModifierProvider(output, registriesFuture).setForgedModLoader(ForgedModLoaders.NEOFORGE));
+    pack.addProvider((o, rf) -> new ModForgeBiomeModifierProvider(o, rf).setForgedModLoader(ForgedModLoaders.NEOFORGE));
+
+    pack.addProvider(CrowdinLanguageProvider::defaultLanguage);
+    pack.addProvider((o, rf) -> CrowdinLanguageProvider.language(o, rf, "pt_br", "pt-BR"));
+    //pack.addProvider((o, rf) -> CrowdinLanguageProvider.language(o, rf, "be_by", "be"));
+    //pack.addProvider((o, rf) -> CrowdinLanguageProvider.language(o, rf, "zn_cn", "zn-CN"));
+    //pack.addProvider((o, rf) -> CrowdinLanguageProvider.language(o, rf, "fr_fr", "fr"));
+    //pack.addProvider((o, rf) -> CrowdinLanguageProvider.language(o, rf, "de_de", "de"));
+    //pack.addProvider((o, rf) -> CrowdinLanguageProvider.language(o, rf, "it_it", "it"));
+    //pack.addProvider((o, rf) -> CrowdinLanguageProvider.language(o, rf, "ja_jp", "ja"));
+    //pack.addProvider((o, rf) -> CrowdinLanguageProvider.language(o, rf, "pl_pl", "pl"));
+    //pack.addProvider((o, rf) -> CrowdinLanguageProvider.language(o, rf, "ru_ru", "ru"));
+    //pack.addProvider((o, rf) -> CrowdinLanguageProvider.language(o, rf, "es_es", "es-ES"));
+    //pack.addProvider((o, rf) -> CrowdinLanguageProvider.language(o, rf, "tr_tr", "tr"));
+    //pack.addProvider((o, rf) -> CrowdinLanguageProvider.language(o, rf, "uk_ua", "uk"));
   }
 }
